67e3e069106563be23db75ab009e7c6711c4ead9
Sergey Leventiy <101974057+LeventiySergey@users.noreply.github.com>
2023-12-05 01:23:21 +0200

    Update 3-Program-Classes-Example.sql

--- 3-SoftwareConstruction/3-Program-Classes-Example.sql
+++ 3-SoftwareConstruction/3-Program-Classes-Example.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE TYPE Payment_type as(
-    payId INT,
+    payId NUMBER,
     paymentMethod VARCHAR(50)
 );
 /
 CREATE OR REPLACE TYPE MeteoService_type as(
-    meteosvcId INT,
+    meteosvcId NUMBER,
     meteosvcName VARCHAR(100),
     cost INT,
     dateTime DATE,
@@ -12,12 +12,12 @@ CREATE OR REPLACE TYPE MeteoService_type as(
 );
 /
 CREATE OR REPLACE TYPE OnlineApplication_type as(
-    appId INT,
+    appId NUMBER,
     userData VARCHAR(1000)
 );
 /
 CREATE OR REPLACE TYPE User_type as(
-    userId INT,
+    userId NUMBER,
     fullName VARCHAR(120),
     address VARCHAR(200),
     accountNumber INT,

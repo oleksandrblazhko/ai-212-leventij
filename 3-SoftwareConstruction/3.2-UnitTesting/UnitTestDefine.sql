a01e227f9003b164e9602fa04ab615d2645be666
Sergey Leventiy <101974057+LeventiySergey@users.noreply.github.com>
2023-12-07 14:33:13 +0200

    Update UnitTestDefine.sql

--- 3-SoftwareConstruction/3.2-UnitTesting/UnitTestDefine.sql
+++ 3-SoftwareConstruction/3.2-UnitTesting/UnitTestDefine.sql
@@ -7,7 +7,7 @@ BEGIN
    
    -- TC 1.1: Валідні дані. Результат: 1
    DBMS_OUTPUT.PUT('TC 1.1: ');
-   result := createRequest('Jane Doe', '123 Main St', 'Sky Insight');
+   result := createRequest('Matiiko Stanislav Stanislavovich', '123 Maple Street, Toronto, Ontario, Canada, M5V 2T6', 'Advanced Climate Prediction and Weather Forecasting Service');
    IF result = 1 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -16,7 +16,7 @@ BEGIN
 
    -- TC 1.2: Ім'я відсутнє. Результат: -1
    DBMS_OUTPUT.PUT('TC 1.2: ');
-   result := createRequest(null, '123 Main St', 'Sky Insight');
+   result := createRequest(null, '123 Maple Street, Toronto, Ontario, Canada, M5V 2T6', 'Advanced Climate Prediction and Weather Forecasting Service');
    IF result = -1 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -25,7 +25,7 @@ BEGIN
 
    -- TC 1.3: Ім'я перевищує допустиму довжину. Результат: -1
    DBMS_OUTPUT.PUT('TC 1.3: ');
-   result := createRequest('Jane Doe' || RPAD('X', 500, 'X'), '123 Main St', 'Sky Insight');
+   result := createRequest('Elizabeth Alexandra Mary Windsor Elizabeth Alexandra Mary Windsor Elizabeth Alexandra Mary Windsor Elizabeth Alexandra Mary Windsor Elizabeth', '123 Maple Street, Toronto, Ontario, Canada, M5V 2T6', 'Advanced Climate Prediction and Weather Forecasting Service');
    IF result = -1 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -34,7 +34,7 @@ BEGIN
 
    -- TC 1.4: Ім'я містить недопустимі символи. Результат: -1
    DBMS_OUTPUT.PUT('TC 1.4: ');
-   result := createRequest('Jane Доу', '123 Main St', 'Sky Insight');
+   result := createRequest('Матійко Stanislav Stanislavovich', '123 Maple Street, Toronto, Ontario, Canada, M5V 2T6', 'Advanced Climate Prediction and Weather Forecasting Service');
    IF result = -1 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -43,7 +43,7 @@ BEGIN
 
    -- TC 1.5: Адреса відсутня. Результат: -2
    DBMS_OUTPUT.PUT('TC 1.5: ');
-   result := createRequest('Jane Doe', null, 'Sky Insight');
+   result := createRequest('Matiiko Stanislav Stanislavovich', null, 'Advanced Climate Prediction and Weather Forecasting Service');
    IF result = -2 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -52,7 +52,7 @@ BEGIN
 
    -- TC 1.6: Адреса перевищує допустиму довжину. Результат: -2
    DBMS_OUTPUT.PUT('TC 1.6: ');
-   result := createRequest('Jane Doe', '123 Main St'|| RPAD('X', 500, 'X'), 'Sky Insight');
+   result := createRequest('Matiiko Stanislav Stanislavovich', '1234 Long Street Name, Very Big City Name, Extremely Large Province Name, Country With A Very Long Name, Postal Code 12345-6789. This is a second line of the address which includes Building Name, Office Number, Additional Directions for the courier to find the location easily and a third line for any extra information', 'Advanced Climate Prediction and Weather Forecasting Service');
    IF result = -2 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -61,7 +61,7 @@ BEGIN
 
    -- TC 1.7: Адреса містить недопустимі символи. Результат: -2
    DBMS_OUTPUT.PUT('TC 1.7: ');
-   result := createRequest('Jane Doe', '123 Мейн St', 'Sky Insight');
+   result := createRequest('Matiiko Stanislav Stanislavovich', '123 Мейпл Стріт, Toronto, Ontario, Canada, M5V 2T6', 'Advanced Climate Prediction and Weather Forecasting Service');
    IF result = -2 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -70,7 +70,7 @@ BEGIN
 
    -- TC 1.8: Назва послуги відсутня. Результат: -3
    DBMS_OUTPUT.PUT('TC 1.8: ');
-   result := createRequest('Jane Doe', '123 Main St', null);
+   result := createRequest('Matiiko Stanislav Stanislavovich', '123 Maple Street, Toronto, Ontario, Canada, M5V 2T6', null);
    IF result = -3 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -79,7 +79,7 @@ BEGIN
 
    -- TC 1.9: Назва послуги перевищує допустиму довжину. Результат: -3
    DBMS_OUTPUT.PUT('TC 1.9: ');
-   result := createRequest('Jane Doe', '123 Main St', 'Sky Insight'|| RPAD('X', 500, 'X'));
+   result := createRequest('Matiiko Stanislav Stanislavovich', '123 Maple Street, Toronto, Ontario, Canada, M5V 2T6', 'Advanced Climate Prediction and Weather Forecasting Service with Specialized Reports for Agriculture and Aviation Industries');
    IF result = -3 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
@@ -88,7 +88,7 @@ BEGIN
 
    -- TC 1.10: Назва послуги містить недопустимі символи. Результат: -3
    DBMS_OUTPUT.PUT('TC 1.10: ');
-   result := createRequest('Jane Doe', '123 Main St', 'Sky Інсайт');
+   result := createRequest('Matiiko Stanislav Stanislavovich', '123 Maple Street, Toronto, Ontario, Canada, M5V 2T6', 'Адвансед Climate Prediction and Weather Forecasting Service');
    IF result = -3 THEN
       DBMS_OUTPUT.PUT_LINE('Passed');
    ELSE
